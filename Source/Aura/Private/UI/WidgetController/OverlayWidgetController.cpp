a2c96a68ab28903ccff3d30955453d52e949e381
mdalcaraz <mdalcaraz@live.com>
2024-01-05 17:26:50 -0300

    Initial health values on widget working

--- Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -3,3 +3,12 @@
 
 #include "UI/WidgetController/OverlayWidgetController.h"
 
+#include "AbilitySystem/AuraAttributeSet.h"
+
+void UOverlayWidgetController::BroadcastInitialVaLues()
+{
+	const UAuraAttributeSet* AuraAttributeSet = CastChecked<UAuraAttributeSet>(AttributeSet);
+
+	OnHealtChanged.Broadcast(AuraAttributeSet->GetHealth());
+	OnMaxHealtChanged.Broadcast(AuraAttributeSet->GetMaxHealth());
+}
