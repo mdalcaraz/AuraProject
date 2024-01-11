467191a9fa6189e6c56bdd793507ed5c42386a2c
mdalcaraz <mdalcaraz@live.com>
2024-01-11 00:43:02 -0300

    Periodic Gameplay Effects

--- Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -29,7 +29,7 @@ void UOverlayWidgetController::BindCallbacksToDependencies()
 	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AuraAttributeSet->GetManaAttribute()).
 	AddUObject(this, &UOverlayWidgetController::ManaChanged);
 	
-	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AuraAttributeSet->GetMaxHealthAttribute()).
+	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AuraAttributeSet->GetMaxManaAttribute()).
 	AddUObject(this, &UOverlayWidgetController::MaxManaChanged);
 
 }
