24f2e443e99c49a4c2b4c70e71994542a75b391e
mdalcaraz <mdalcaraz@live.com>
2024-05-12 01:10:33 -0300

    receiving target data

--- Source/Aura/Public/AbilitySystem/AbilityTasks/TargetDataUnderMouse.h
+++ Source/Aura/Public/AbilitySystem/AbilityTasks/TargetDataUnderMouse.h
@@ -6,7 +6,7 @@
 #include "Abilities/Tasks/AbilityTask.h"
 #include "TargetDataUnderMouse.generated.h"
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMouseTargetDataSignaure, const FVector&, Data);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMouseTargetDataSignaure, const FGameplayAbilityTargetDataHandle&, DataHandle);
 /**
  * 
  */
@@ -25,4 +25,8 @@ public:
 
 private:
 	virtual void Activate() override;
+
+	void SendMouseCursorData();
+
+	void OnTargetDataReplicatedCallback(const FGameplayAbilityTargetDataHandle& DataHandle, FGameplayTag ActivationTag);
 };
